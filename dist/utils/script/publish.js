b0195e61e758c637ce7376da21ac39bb80a7ea3c
Pedro Augusto <pedro007augustobarbosa@gmail.com>
2022-02-05 15:41:10 -0300

    Testing piblish script

--- dist/utils/script/publish.js
+++ dist/utils/script/publish.js
@@ -1,71 +1,52 @@
-import readline from "readline";
-
-import { exec } from "child_process";
-
-const line = readline.createInterface({
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const readline_1 = __importDefault(require("readline"));
+const child_process_1 = require("child_process");
+const line = readline_1.default.createInterface({
     input: process.stdin,
     output: process.stdout
-
 });
-
 try {
-    exec("yarn build", (err, stdout, stderr) => {
+    (0, child_process_1.exec)("yarn build", (err, stdout, stderr) => {
         if (err) {
             console.log(`Code: ${err.name} - ${err.code} - ${err.cmd}`);
             throw new Error(err.message);
-
         }
-
-        if (stderr) 
+        if (stderr)
             throw new Error(stderr);
-
         console.log(stdout);
-
         line.question("Put your commit message (put the message between \"\" or \'\'): ", (answer) => {
             if (answer) {
-                exec(`git add . && git commit -m ${answer}`, (_err, _stdout, _stderr) => {
+                (0, child_process_1.exec)(`git add . && git commit -m ${answer}`, (_err, _stdout, _stderr) => {
                     if (_err) {
                         console.log(`Code: ${_err.name} - ${_err.code} - ${_err.cmd}`);
                         throw new Error(_err.message);
-            
                     }
-            
-                    if (_stderr) 
+                    if (_stderr)
                         throw new Error(_stderr);
-            
                     console.log(_stdout);
-
                     line.question("Put your branch: ", (_answer) => {
                         if (answer) {
-                            exec(`git push -u origin ${_answer}`, (__err, __stdout, __stderr) => {
+                            (0, child_process_1.exec)(`git push -u origin ${_answer}`, (__err, __stdout, __stderr) => {
                                 if (__err) {
                                     console.log(`Code: ${__err.name} - ${__err.code} - ${__err.cmd}`);
                                     throw new Error(__err.message);
-                        
                                 }
-                        
-                                if (__stderr) 
+                                if (__stderr)
                                     throw new Error(__stderr);
-                        
                                 console.log(__stdout);
-            
                                 process.exit(0);
-            
                             });
-            
                         }
-            
                     });
-
                 });
-
             }
-
         });
-
     });
-
-} catch (err) {
+}
+catch (err) {
     console.log(err);
-
 }
